4b5752ec280982aa032d34cf73c3f69c956452e5
Zack Jackson <z.jackson58@gmail.com>
2018-12-01 12:25:56 -0500

    Info section styles

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -1,67 +1,155 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import Zoom from 'react-reveal/Zoom';
 
 const EventInfo = () => {
   return (
     <div className="event-info">
-      <Zoom duration={400}>
-        <Card raised="true">
-          <CardHeader
-            title="Arctic Monkeys"
-            subheader="with Mini Mansions and The Lemon Twigs"
-          />
-          <CardContent>
-            <div>Friday Nov, 8, 2019 @ 8:00PM</div>
-            <div>All ages. 21+ to drink.</div>
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-      </Zoom>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={40}
+      >
+        <Grid item xs={3}>
+          <Zoom duration={400}>
+            <Card raised="true" className="event-info__card">
+              <CardContent className="event-info__card-header">
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-title"
+                >
+                  Arctic Monkeys
+                </Typography>
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-subtitle"
+                >
+                  with Mini Mansions
+                </Typography>
+              </CardContent>
+              <Divider variant="middle" />
+              <CardContent className="event-info__card-body">
+                <div className="event-info__card-body-text">
+                  Friday Nov, 8, 2019 @ 8:00PM
+                </div>
+                <div className="event-info__card-body-text">
+                  All ages. 21+ to drink.
+                </div>
+              </CardContent>
+              <CardActions className="event-info__action">
+                <Button
+                  variant="contained"
+                  size="large"
+                  color="primary"
+                  className="event-info__button"
+                >
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Zoom>
+        </Grid>
 
-      <Zoom duration={400} delay={400}>
-        <Card raised="true">
-          <CardHeader title="The Canal Club" subheader="Richmond, Virginia" />
-          <CardContent>
-            <div>1545 East Cary Street Richmond, VA 23219</div>
-            <div>Phone: 804-643-2582</div>
-            <div>Email: Info@TheCanalClub.com</div>
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary">
-              Menu
-            </Button>
-            <Button size="small" color="primary">
-              FAQ
-            </Button>
-          </CardActions>
-        </Card>
-      </Zoom>
+        <Grid item xs={3}>
+          <Zoom duration={400} delay={400}>
+            <Card raised="true" className="event-info__card">
+              <CardContent className="event-info__card-header">
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-title"
+                >
+                  Tickets
+                </Typography>
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-subtitle"
+                >
+                  Powered by TicketFly
+                </Typography>
+              </CardContent>
+              <Divider variant="middle" />
+              <CardContent className="event-info__card-body">
+                <div className="event-info__card-body-text">
+                  Available for purchase through TicketFly.
+                </div>
+                <div className="event-info__card-body-text">
+                  Click the link below for pricing.
+                </div>
+              </CardContent>
+              <CardActions className="event-info__action">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className="event-info__button"
+                >
+                  Get Tickets
+                </Button>
+              </CardActions>
+            </Card>
+          </Zoom>
+        </Grid>
 
-      <Zoom duration={400} delay={800}>
-        <Card raised="false">
-          <CardHeader title="Tickets" subheader="Powered by TicketFly" />
-          <CardContent>
-            <div>Available online through TicketFly. Click the link below.</div>
-            <div>
-              Tickets available for purchase at the event may be limited.
-            </div>
-          </CardContent>
-          <CardActions>
-            <Button size="small" color="primary">
-              Get Tickets
-            </Button>
-          </CardActions>
-        </Card>
-      </Zoom>
+        <Grid item xs={3}>
+          <Zoom duration={400} delay={800}>
+            <Card raised="true" className="event-info__card">
+              <CardContent className="event-info__card-header">
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-title"
+                >
+                  The Canal Club
+                </Typography>
+                <Typography
+                  gutterBottom="true"
+                  className="event-info__card-header-subtitle"
+                >
+                  Richmond, Virginia
+                </Typography>
+              </CardContent>
+              <Divider variant="middle" />
+              <CardContent className="event-info__card-body">
+                <div className="event-info__card-body-text">
+                  1545 East Cary Street Richmond, VA 23219
+                </div>
+                <div className="event-info__card-body-text">
+                  Phone: 804-643-2582
+                </div>
+                <div className="event-info__card-body-text">
+                  Email: Info@TheCanalClub.com
+                </div>
+              </CardContent>
+              <CardActions className="event-info__action">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className="event-info__button"
+                >
+                  FAQ
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  size="large"
+                  className="event-info__button"
+                >
+                  Menu
+                </Button>
+              </CardActions>
+            </Card>
+          </Zoom>
+        </Grid>
+      </Grid>
     </div>
   );
 };
