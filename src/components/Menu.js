d8ac48b73a792a6e6d84edcad3b6e11cdfc862e1
Zack Jackson <z.jackson58@gmail.com>
2018-12-10 22:06:31 -0500

    Menu styles

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,6 +4,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
 
 import MenuItem from './MenuItem';
 
@@ -15,21 +17,27 @@ const Menu = ({ dialogOpen, dialogClose }) => {
       price: 2.95
     },
     {
+      title: 'Nachos',
+      description:
+        'a large plate of tortilla chips topped with cheese, your choice of chicken or beef, diced lettuce and tomato, salsa, sour cream, and jalapenos',
+      price: 7.95
+    },
+    {
       title: 'Onion Rings',
       description: 'beer battered',
       price: 3.95
+    },
+    {
+      title: 'Soft Drinks',
+      description: 'Coke, Diet Coke, Sprite, Root Beer - 16oz',
+      price: 2.95
     },
     {
       title: 'Mozzarella Sticks',
       description: 'served with marinara sauce',
       price: 4.95
     },
-    {
-      title: 'Nachos',
-      description:
-        'a large plate of tortilla chips topped with cheese, your choice of chicken or beef, diced lettuce and tomato, salsa, sour cream, and jalapenos',
-      price: 7.95
-    },
+    ,
     {
       title: 'Quesadilla',
       description:
@@ -38,14 +46,10 @@ const Menu = ({ dialogOpen, dialogClose }) => {
     },
     {
       title: 'Chicken Wings',
-      description: 'Traditional wings served with a side of ranch dressing',
+      description: 'served with a side of ranch dressing',
       price: 5.95
     },
-    {
-      title: 'Soft Drinks',
-      description: 'Coke, Diet Coke, Sprite, Root Beer - 16oz',
-      price: 2.95
-    },
+
     {
       title: 'Beer',
       description: 'Bud Light, Coors Light, Pabst Blue Ribbon - 12oz',
@@ -53,31 +57,57 @@ const Menu = ({ dialogOpen, dialogClose }) => {
     }
   ];
 
-  const renderMenuItems = menu.map(({ title, description, price }) => {
+  const renderMenuItems = () => {
+    const colSplitPoint = Math.floor(menu.length / 2);
+
     return (
-      <MenuItem
-        key={title}
-        title={title}
-        description={description}
-        price={price}
-      />
+      <Grid container spacing={40}>
+        <Grid item xs={6}>
+          {menu.slice(0, colSplitPoint).map(({ title, description, price }) => (
+            <MenuItem
+              key={title}
+              title={title}
+              description={description}
+              price={price}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={6}>
+          {menu.slice(colSplitPoint).map(({ title, description, price }) => (
+            <MenuItem
+              key={title}
+              title={title}
+              description={description}
+              price={price}
+            />
+          ))}
+        </Grid>
+      </Grid>
     );
-  });
+  };
 
   return (
     <Dialog
       aria-labelledby="menu"
       aria-describedby="menu"
+      fullWidth={true}
+      maxWidth="md"
       open={dialogOpen}
+      onBackdropClick={dialogClose}
       className="menu"
     >
-      <DialogTitle className="menu__title">
-        <div className="menu__header">Menu</div>
-        <IconButton onClick={dialogClose}>
-          <Close />
-        </IconButton>
+      <DialogTitle>
+        <div className="menu__title">
+          <div className="menu__header">Menu</div>
+          <IconButton onClick={dialogClose} className="menu__button">
+            <Close />
+          </IconButton>
+        </div>
       </DialogTitle>
-      <DialogContent className="menu__items">{renderMenuItems}</DialogContent>
+      <Divider variant="middle" />
+      <DialogContent>
+        <div className="menu__items">{renderMenuItems()}</div>
+      </DialogContent>
     </Dialog>
   );
 };
